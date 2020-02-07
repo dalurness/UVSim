ed7cdabac8fab6b7544f797c1bbcfa0e412b8b75
dalurness <43400680+dalurness@users.noreply.github.com>
2020-02-06 18:00:07 -0700

    add some pseudocode

--- UVSim/UVSim/Source.cpp
+++ UVSim/UVSim/Source.cpp
@@ -9,3 +9,43 @@ int main() {
 	anotherNewFunction();
 	return 0;
 }
+
+
+/*
+SOME SUEDOCODE FOR THE ORGANIZATION/REQUIREMENTS OF THE PROJECT
+
+	class sim
+		int Accumulator
+		int InstructionCounter
+		int InstructionRegister
+		int OperationCode
+		int Operand
+		vector[100] //memory size
+
+	//potentially have the vector grow with the instructions instead of just declaring
+	//	it with 100 locations to save memory. This is reliant on the idea that
+	//	the user must create their own memory locations for storage like 00000
+
+	//functions
+	printMemory()
+	getDataFromUser()
+	getDataFromFile()
+	executeProgram()
+	//internal functions
+	read()
+	write()
+	load()
+	store()
+	add()
+	subtract()
+	divide()
+	multiply()
+	branch()
+	branchNeg()
+	branchZero()
+	halt()
+	MEMDUMP()
+	BREAK()
+	CONTINUE()
+
+*/
